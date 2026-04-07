1403cf97da8159c412c8043c1943925d3dd7473c
Lennart Heinrich <lennart.heinrich@edu.fhdw.de>
2026-04-07 14:05:41 +0200

    package constant screaming case

--- common/prng.vhd
+++ common/prng.vhd
@@ -4,9 +4,9 @@ use ieee.numeric_std.all;
 
 package prng is
 
-    constant generator32 : std_logic_vector(32 downto 0)
+    constant GENERATOR32 : std_logic_vector(32 downto 0)
         := (32 => '1', 22 => '1', 2 => '1', 1 => '1', 0 => '1', others => '0');
-    constant seed32 : std_logic_vector(31 downto 0)
+    constant SEED32 : std_logic_vector(31 downto 0)
         := "11010010000101011011000111111001";
 
     component lfsr
