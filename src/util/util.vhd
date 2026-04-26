93619ddc72d8d86314adbfac44ed388562b56d9c
Lennart Heinrich <lennart.heinrich@edu.fhdw.de>
2026-04-26 14:28:12 +0200

    fix util formatting

--- src/util/util.vhd
+++ src/util/util.vhd
@@ -4,32 +4,32 @@ use ieee.numeric_std.all;
 
 package util is
 
-	 function to_string(vec : std_logic_vector)
-	 return string;
+    function to_string(vec : std_logic_vector)
+    return string;
 
-	 function to_string(vec : signed)
-	 return string;
-	 
-	 function to_string(vec : unsigned)
-	 return string;
+    function to_string(vec : signed)
+    return string;
+    
+    function to_string(vec : unsigned)
+    return string;
 
-    procedure print (vec : std_logic_vector);
+    procedure print(vec : std_logic_vector);
 
-    procedure print (vec : signed);
+    procedure print(vec : signed);
 
-    procedure print (vec : unsigned);
+    procedure print(vec : unsigned);
 
 end package;
 
 package body util is
 
-	 function to_string(vec : std_logic_vector)
-	 return string is
-	 	 variable str : string(1 to vec'length);
-	 	 variable str_i : integer := 1;
-	 begin
-	 	 for i in vec'range loop
-       	 case vec(i) is
+    function to_string(vec : std_logic_vector)
+    return string is
+        variable str : string(1 to vec'length);
+        variable str_i : integer := 1;
+    begin
+        for i in vec'range loop
+        case vec(i) is
                 when '0' => str(str_i) := '0';
                 when '1' => str(str_i) := '1';
                 when 'U' => str(str_i) := 'U';
@@ -46,29 +46,29 @@ package body util is
         return str;
 	 end function;
 
-	 function to_string(vec : signed)
-	 return string is
-	 begin
-	 	  return to_string(std_logic_vector(vec));
-	 end function;
+    function to_string(vec : signed)
+    return string is
+    begin
+    return to_string(std_logic_vector(vec));
+    end function;
 
-	 function to_string(vec : unsigned)
-	 return string is
-	 begin
-	 	  return to_string(std_logic_vector(vec));
-	 end function;
+    function to_string(vec : unsigned)
+    return string is
+    begin
+        return to_string(std_logic_vector(vec));
+    end function;
 
-    procedure print (vec : std_logic_vector) is
+    procedure print(vec : std_logic_vector) is
     begin
         report to_string(unsigned(vec));
     end procedure;
 
-    procedure print (vec : signed) is
+    procedure print(vec : signed) is
     begin
         report to_string(unsigned(vec));
     end procedure;
     
-    procedure print (vec : unsigned) is
+    procedure print(vec : unsigned) is
     begin
         report to_string(vec);
     end procedure;
