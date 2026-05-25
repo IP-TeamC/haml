44be2940c394f8f1c669a79e214fdeb9ea4fc09c
Lennart Heinrich <lennart.heinrich@edu.fhdw.de>
2026-05-25 16:55:38 +0200

    fix fitness_linreg_tb

--- test/ga_linreg/fitness_linreg_tb.vhd
+++ test/ga_linreg/fitness_linreg_tb.vhd
@@ -14,7 +14,7 @@ entity fitness_linreg_tb is
     constant var_num : natural := 2;
     constant fp_size : natural := 8;
     constant fp_frac : natural := 6;
-    constant fit_size : natural := fp_size;
+    constant fit_size : natural := 2*fp_size;
     constant adr_size : natural := 2;
     constant square : boolean := false;
 
@@ -28,7 +28,7 @@ entity fitness_linreg_tb is
 
     -- Outputs
     signal ram_dp_adr : std_logic_vector(adr_size-1 downto 0);
-    signal fit : std_logic_vector(fp_size-1 downto 0);
+    signal fit : std_logic_vector(fit_size-1 downto 0);
     signal done : std_logic;
 
     -- Setup
@@ -86,12 +86,15 @@ begin
     end process;
 
     process
+        variable tmp1 : std_logic_vector(fit_size-1 downto 0);
+        variable tmp2 : std_logic_vector(fit_size-1 downto 0);
     begin
         rst <= '1';
         wait for clk_period;
         rst <= '0';
         assert done = '0';
 
+        start <= '0';
         ram_dp_we <= '1';
         ram_dp_adr_write <= "00";
         -- x2 = 0.1875, x1 = 0.3828125, y = 0.28125 (nicht ganz exakt)
@@ -100,6 +103,7 @@ begin
             & "00" & "011001"
             & "00" & "010010";
         wait for clk_period;
+        assert done = '0';
         ram_dp_adr_write <= "01";
         -- x2 = 0.0625, x1 = 0.5, y = 0.21875 (exakt)
         -- kein Error
@@ -107,6 +111,7 @@ begin
             & "00" & "100000"
             & "00" & "001110";
         wait for clk_period;
+        assert done = '0';
         ram_dp_adr_write <= "10";
         -- x2 = 0.0625, x1 = 0.5, y = 0.875 (schlecht)
         -- Error: 0.430664063
@@ -114,6 +119,7 @@ begin
             & "00" & "100000"
             & "00" & "111000";
         wait for clk_period;
+        assert done = '0';
         ram_dp_we <= '0';
 
         start <= '1';
@@ -133,19 +139,26 @@ begin
         assert done = '0';
         assert ram_dp_adr = "10";
 
-        wait until done = '1' and clk = '0';
-        -- kleiner Fehler (zu ungenau, deshalb 0)
-        assert fit = "00000000";
-        wait for clk_period;
+        while done = '0' loop
+            tmp1 := tmp2;
+            tmp2 := fit;
+            wait until falling_edge(clk);
+        end loop;
         assert done = '1';
-        -- unveraenderter Fehler
-        assert fit = "00000000";
-        wait for clk_period;
-        assert done = '1';
-        -- viel groeßerer Fehler
-        assert fit = "00" & "000110";
-        wait for clk_period;
-        assert done = '0';
+
+        -- kleiner Fehler für 1. DP
+        assert tmp1 = "0000000000000111";
+        -- unveraenderter Fehler für 2. DP
+        assert tmp2 = tmp1;
+        -- viel groeßerer Fehler für 3. DP
+        assert fit = "0000000101010111";
+
+        -- stabile Ausgabe, wenn start = 0
+        for i in 0 to 30 loop
+            wait for clk_period;
+            assert done = '0';
+            assert fit = "0000000101010111";
+        end loop;
 
         report "Done";
         wait;
