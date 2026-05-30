c204f8808b942ad9f00079b822fab0f5f8c98128
Lennart Heinrich <lennart.heinrich@edu.fhdw.de>
2026-05-30 14:54:23 +0200

    timing constraints + cleanup xise project

--- src/math/signed_dist.vhd
+++ src/math/signed_dist.vhd
@@ -36,13 +36,16 @@ end entity;
 -- Pipeline: Differenz -> Quadrat -> Addition (Adder Tree mit mehreren Stages)
 architecture rtl of signed_dist is
 
-    signal diff : std_logic_vector(n*fp_size-1 downto 0);
-    signal diff_next : std_logic_vector(n*fp_size-1 downto 0);
+    type t_diff is array (0 to n-1) of signed(fp_size-1 downto 0);
+    signal diff : t_diff;
+    signal diff_next : t_diff;
     signal done_diff : std_logic;
     signal di_delayed_diff : std_logic_vector(data_size-1 downto 0);
 
-    signal diff_sq : std_logic_vector(n*calc_signed_dist_sq_size(fp_size, extend)-1 downto 0);
-    signal diff_sq_next : std_logic_vector(n*calc_signed_dist_sq_size(fp_size, extend)-1 downto 0);
+    type t_diff_sq is array (0 to n-1) of signed(calc_signed_dist_sq_size(fp_size, extend)-1 downto 0);
+    signal diff_sq : t_diff_sq;
+    signal diff_sq_next : t_diff_sq;
+    signal diff_sq_flat : std_logic_vector(n*calc_signed_dist_sq_size(fp_size, extend)-1 downto 0);
     signal done_diff_sq : std_logic;
     signal di_delayed_diff_sq : std_logic_vector(data_size-1 downto 0);
 
@@ -59,7 +62,7 @@ begin
             clk => clk,
             rst => rst,
             start => done_diff_sq,
-            values => diff_sq,
+            values => diff_sq_flat,
             sum => dist_sq,
             done => done,
             di => di_delayed_diff_sq,
@@ -69,20 +72,18 @@ begin
     process (clk)
     begin
         if rising_edge(clk) then
+            if start = '1' then
+                diff <= diff_next;
+                di_delayed_diff <= di;
+            end if;
+
+            diff_sq <= diff_sq_next;
+            di_delayed_diff_sq <= di_delayed_diff;
+
             if rst = '1' then
                 done_diff <= '0';
                 done_diff_sq <= '0';
-            else -- Weitergabe das Start-Signals an die folgenden Pipeline-Stufen sowie Weitergabe der Daten di -> do
-                if start = '1' then
-                    diff <= diff_next;
-                    di_delayed_diff <= di;
-                end if;
-
-                if done_diff = '1' then
-                    diff_sq <= diff_sq_next;
-                    di_delayed_diff_sq <= di_delayed_diff;
-                end if;
-
+            else
                 done_diff <= start;
                 done_diff_sq <= done_diff;
             end if;
@@ -92,7 +93,7 @@ begin
     -- Stage 1: Berechnung der Differenzen
     gen_diff: for i in 0 to n-1 generate
     begin
-        diff_next(flat_upper(fp_size, i) downto flat_lower(fp_size, i)) <= std_logic_vector(flat_signed(a, fp_size, i) - flat_signed(b, fp_size, i));
+        diff_next(i) <= flat_signed(a, fp_size, i) - flat_signed(b, fp_size, i);
     end generate;
 
     -- Stage 2: Quadrieren der Differenzen
@@ -100,15 +101,24 @@ begin
     begin
 
         gen_diff_sq_extend: if extend = true generate
-            diff_sq_next(flat_upper(2*fp_size, i) downto flat_lower(2*fp_size, i)) <= std_logic_vector(flat_signed(diff, fp_size, i) * flat_signed(diff, fp_size, i));
+            diff_sq_next(i) <= diff(i) * diff(i);
         end generate;
 
         gen_diff_sq_noextend: if extend = false generate
-            diff_sq_next(flat_upper(fp_size, i) downto flat_lower(fp_size, i)) <= std_logic_vector(fp_mul(
-                flat_signed(diff, fp_size, i),
-                flat_signed(diff, fp_size, i),
-                fp_frac
-            ));
+            diff_sq_next(i) <= fp_mul(diff(i), diff(i), fp_frac);
+        end generate;
+
+    end generate;
+
+    -- Vorbereitung Stage 3: Flat-Vektor erstellen
+    gen_diff_sq_flat: for i in 0 to n-1 generate
+
+        gen_diff_sq_flat_extend: if extend = true generate
+            diff_sq_flat(flat_upper(2*fp_size, i) downto flat_lower(2*fp_size, i)) <= std_logic_vector(diff_sq(i));
+        end generate;
+
+        gen_diff_sq_flat_noextend: if extend = false generate
+            diff_sq_flat(flat_upper(fp_size, i) downto flat_lower(fp_size, i)) <= std_logic_vector(diff_sq(i));
         end generate;
 
     end generate;
